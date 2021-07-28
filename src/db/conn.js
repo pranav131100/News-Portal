c5f8a4c339a80b996742994bd7d91405144005b6
pranav131100 <pranav.shirke1311@gmail.com>
2021-07-28 23:51:13 +0530

    conn.js changed

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongoURL = process.env.MONGODB_URL || "mongodb://localhost:27017/registration"
-mongoose.connect(mongoURL,{
+mongoose.connect("mongodb+srv://pranav00:%401311@gmail.com@cluster0.g8ppu.mongodb.net/terminal",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
