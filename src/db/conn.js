8672328f8ce1d18d4f0f21d49909090a310cae1e
pranav131100 <pranav.shirke1311@gmail.com>
2021-07-29 22:26:09 +0530

    minute change

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,8 @@ const mongoURL = process.env.MONGODB_URL || "mongodb://localhost:27017/registrat
 mongoose.connect("mongodb+srv://abc:abcPranav123@cluster0.to716.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
     
 }).then(()=>{
     console.log("Connection successful.");
