db9f7d543f4ded8ee1ec6709b4108ecf2e5387e7
pranav131100 <pranav.shirke1311@gmail.com>
2021-08-02 08:45:19 +0530

    conn.js changed

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const mongoURL = "mongodb+srv://abc:abcPranav123@cluster0.to716.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" || "mongodb://localhost:27017/registration"
-mongoose.connect("mongodb+srv://xyz:Pranavxyz@cluster0.hlgpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://abc:abcPranav123@cluster0.to716.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
     
 }).then(()=>{
     console.log("Connection successful.");
